abdc5d9e94cb9f2132c04e81176f23264a58820e
radib <radibkar32@gmail.com>
2020-05-14 17:58:13 +0530

    project setup done

---
+++ app/src/main/java/com/mindorks/ridesharing/utils/PermissionUtils.kt
@@ -0,0 +1,31 @@
+package com.mindorks.ridesharing.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+object PermissionUtils {
+    fun reqestAccesssFindLocationPermission(activity: AppCompatActivity,requestId: Int){
+        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),requestId)
+    }
+    fun isAccessFineLocationGranted(context: Context):Boolean{
+        return ContextCompat.checkSelfPermission(context,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED
+    }
+    fun isLocationEnabled(context: Context):Boolean{
+        val locationManager:LocationManager=context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled((LocationManager.NETWORK_PROVIDER))
+    }
+
+    fun showGPSNOTEnable(context: Context){
+        AlertDialog.Builder(context)
+            .setTitle("Enable Gps")
+
+    }
+
+
+}
